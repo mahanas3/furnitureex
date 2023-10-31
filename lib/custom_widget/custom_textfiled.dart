6437661c0117a91ad5577d0f1649bede6433c4be
Mahanas <mahanasajmal@gmail.com>
2023-10-31 13:26:07 +0530

    updated

---
+++ lib/custom_widget/custom_textfiled.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CustomTextfield extends StatelessWidget {
+  CustomTextfield(
+      {super.key,
+      required this.text1,
+      required this.text2,
+      required this.icon});
+
+  String text1;
+  String text2;
+  Widget icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      style: const TextStyle(fontSize: 20, fontFamily: 'popins'),
+      decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.black.withOpacity(0.2))),
+          focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(
+            color: Colors.black,
+          )),
+          fillColor: Colors.white.withOpacity(0.2),
+          filled: true,
+          labelText: text1,
+          labelStyle: const TextStyle(
+              color: Colors.black87, fontSize: 20, fontFamily: 'popins'),
+          hintText: text2,
+          hintStyle: const TextStyle(
+              color: Colors.black87, fontFamily: 'popins', fontSize: 20),
+          prefixIcon: icon,
+          prefixIconColor: Colors.black87),
+    );
+  }
+}
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:furnitureex/custom_widget/custom_textfiled.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
@@ -6,38 +7,78 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Stack(children: [
-            Container(
-              height: 756,
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                  image: DecorationImage(
-                      image: NetworkImage(
-                          'https://images.unsplash.com/photo-1586023492125-27b2c045efd7?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZnVybml0dXJlfGVufDB8fDB8fHww'),
-                      fit: BoxFit.fill)),
-            ),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 250),
-                child: Container(
-                  height: 300,
-                  width: 300,
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.all(Radius.circular(20)),
-                    border: Border.all(color: Colors.white),
-                  ),
-                  child: TextField(
-                      decoration: InputDecoration(filled: true,
-                          fillColor: Colors.white.withOpacity(0.5),
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20)))),
-                ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(children: [
+              Container(
+                height: 756,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            'https://images.unsplash.com/photo-1586023492125-27b2c045efd7?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZnVybml0dXJlfGVufDB8fDB8fHww'),
+                        fit: BoxFit.fill)),
               ),
-            )
-          ])
-        ],
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 250),
+                  child: Container(
+                    height: 300,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(20)),
+                      border: Border.all(color: Colors.white),
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              top: 50, left: 15, right: 15),
+                          child: CustomTextfield(
+                            text1: 'Email',
+                            text2: 'Email',
+                            icon: const Icon(Icons.email_outlined),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15, right: 15),
+                          child: CustomTextfield(
+                              text1: 'Password',
+                              text2: 'Password',
+                              icon: const Icon(Icons.password)),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        SizedBox(
+                          height: 40,
+                          width: 100,
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor:
+                                      Colors.white.withOpacity(0.4)),
+                              onPressed: () {},
+                              child: const Text(
+                                'Login',
+                                style: TextStyle(
+                                    color: Colors.black87,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                    fontFamily: 'popins'),
+                              )),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ])
+          ],
+        ),
       ),
     );
   }
